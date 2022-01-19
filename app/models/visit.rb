e056f058a10aadafe0f726d0b539d080ce0f4626
imcarissa <carissa.sarreal@gmail.com>
2022-01-19 07:05:17 -0500

    Root route established and Visit model scope removed

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,3 @@
 class Visit < ApplicationRecord
-    scope :desc, order(created_at: :desc)
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
+  root "visits#index"
+  
   resources :visits
   resources :patients
-  resources :sessions
+ 
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
