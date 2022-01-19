012920cd09698548547e2b5a67cf93f8fde7e622
imcarissa <carissa.sarreal@gmail.com>
2022-01-19 06:23:24 -0500

    sessions table migrated

--- db/migrate/20220119025931_create_sessions.rb
+++ db/migrate/20220119025931_create_sessions.rb
@@ -1,7 +1,13 @@
 class CreateSessions < ActiveRecord::Migration[7.0]
   def change
     create_table :sessions do |t|
-
+      t.string "goal_name"
+      t.datetime "session_date"
+      t.string "word"
+      t.string "plus"
+      t.string "minus"
+      t.string "iwp"
+      t.string "additional notes"
       t.timestamps
     end
   end
